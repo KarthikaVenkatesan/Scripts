29a22c85563ff8a7e2e72f73e4fbf1f84fa79e03
KarthikaVenkatesan <46651010+KarthikaVenkatesan@users.noreply.github.com>
2019-01-13 22:25:25 +0530

    loadSubCategory.js
    OnLoad client script for populating subcategory based on category in Service catalogs.

---
+++ loadSubCategory.js
@@ -0,0 +1,23 @@
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading || newValue == '') {
+        return;
+    }
+	
+	 var ga = new GlideAjax('CategoryHelperAjax1');
+   ga.addParam('sysparm_name','getSubcategories');
+	ga.addParam('sysparm_category', g_form.getValue('category'));
+   ga.getXML(ajaxProcessor);   
+}
+
+function ajaxProcessor(response){
+
+    var answer = response.responseXML.documentElement.getAttribute('answer');
+		  var subcategoryList = JSON.parse(answer);          
+			
+            g_form.clearOptions('subcategory');
+            g_form.addOption('subcategory', '', '-- None --');
+            for (var i = 0; i < subcategoryList.length; i++) {
+                g_form.addOption('subcategory', subcategoryList[i], subcategoryList[i]);
+            }
+
+ }
